942bcbe34f75e3acfbf798923d7223e29945256b
alexbnewman <alex02new@outlook.com>
2023-10-29 17:25:24 -0400

    Added some starter files/removed uploads file.

--- application/config.py
+++ application/config.py
@@ -8,7 +8,6 @@ APPLICATION_ROOT = '/'
 
 # File Upload to var/uploads/
 APPLICATION_ROOT = pathlib.Path(__file__).resolve().parent.parent
-UPLOAD_FOLDER = APPLICATION_ROOT/'var'/'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
