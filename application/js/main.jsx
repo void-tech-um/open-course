074fe4611ede257f19b7596eb5bd9110438f16a4
Jennifer Weir <jennweir@umich.edu>
2023-11-12 12:52:22 -0500

    react entry point working

--- application/js/main.jsx
+++ application/js/main.jsx
@@ -8,22 +8,3 @@ const root = createRoot(document.getElementById("reactEntry"));
 // render application component
 root.render(<App url="/"/>);
 
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Stylesheets/index.css';
-import App from './App';
-import Routes from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
-*/
