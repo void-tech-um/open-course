3145a369dc435382acc59f538814e17e5a590460
tinglu <tinglu@umich.edu>
2024-02-13 19:01:54 -0500

    New Schema

--- psql/schema.sql
+++ psql/schema.sql
@@ -1,24 +1,40 @@
 CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) PRIMARY KEY NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone_num VARCHAR(20) NOT NULL,
     profile_picture VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE posts (
-    postid SERIAL PRIMARY KEY,
-    owner VARCHAR(50),
-    message VARCHAR(350),
-    class VARCHAR(50),
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    "description" VARCHAR(350) NOT NULL,
+    course_code VARCHAR(50) NOT NULL,
     created DATETIME DEFAULT CURRENT_TIMESTAMP, 
-    FOREIGN KEY (owner) REFERENCES users(username) ON DELETE CASCADE
+    schedule_link VARCHAR(100),
+    "type" BOOLEAN NOT NULL,    
+    tag_id INTEGER,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE courses (
-    course_id INT PRIMARY KEY,
-    course_name VARCHAR(50) NOT NULL, 
-    username VARCHAR(50) NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+    course_code VARCHAR(50) PRIMARY KEY NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id) ON DELETE CASCADE,
 );
 
-SELECT * FROM users JOIN courses ON users.username = courses.username;
+CREATE TABLE enrollments (
+    enrollments_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(50) NOT NULL,
+    course_code VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
+    FOREIGN KEY (course_code) REFERENCES courses(course_code) ON DELETE CASCADE
+);
+
+CREATE TABLE tags {
+    tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tag_name VARCHAR(50) NOT NULL UNIQUE
+
+};
