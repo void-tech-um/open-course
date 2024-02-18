e9acc69d2a23d62e4ee8b95539bd4dbeee4d8fa8
tinglu <tinglu@umich.edu>
2024-02-18 15:46:22 -0500

    Fixed comment in join function

--- application/views/join.py
+++ application/views/join.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @application.app.route('/join/<username>/')
 def show_join(username):
-    """Display /profile route."""
+    """Display /join/ route."""
     courses = application.model.get_courses_of_user(username)
     context = {"courses": courses}
     return render_template('join.html', **context)
