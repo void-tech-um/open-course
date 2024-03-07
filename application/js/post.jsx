2d3f5b2a8d3335301f49aa8f12954e23d41e3e25
Becky <beckypan@umich.edu>
2024-03-06 21:42:39 -0500

    styling fixed

--- application/js/post.jsx
+++ application/js/post.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 import '../static/css/style.css';
+
 // STUDY GROUP POST COMPONENT
 
 export default function Post(props) {
@@ -63,32 +64,36 @@ const handleChange = (event) => {
 
 //if (postHasRendered) {
     return (
-    <div class = "post-border">
-        <div class = "user-info">
-            <p class = "user_name">UserName</p>
-            <p class = "email">UmichEmail</p>
-            <input class="star" type="checkbox" title="bookmark page" checked/><br/>
-        </div>
-        <div class = "tags">
-            <button class = "info-tag" type = "button">Tag</button>
-            <button class = "info-tag" type = "button">Tag</button>
-            <button class = "info-tag" type = "button">Tag</button>
-            <button class = "info-tag" type = "button">Tag</button>
-        </div>
-        <div class = "post-info">
-            <p class = "study-group">STUDY GROUP</p>
-            <h1>TITLE</h1>
-            <p class = "brief-descript">Brief Description</p>
-            <p>Date</p>
-            <p class = "label">time</p>
-            <p class = "add-to-calendar">Add to calendar</p>
-            <p>Room</p>
-            <p>Address</p>
-        </div>
-        <div class = "join-section">
-            <h2 class = "join-button">Join</h2>
-        </div>
+    <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+        <div class = "post-border">
+            <div class="user-info profile-right">
+                <div class="circle"></div>
+                <input class="star" type="checkbox" title="bookmark page" checked/> 
+                <p class="user_name">Username </p>
+                <p class="email">UmichEmail</p>
+            </div>
+            <div class = "tags profile-right">
+                <button class = "info-tag" type = "button">Topic</button>
+                <button class = "info-tag tag-spacing" type = "button">Topic</button>
+                <button class = "info-tag tag-spacing" type = "button">Topic</button>
+                <button class = "info-tag tag-spacing" type = "button">Topic</button>
+            </div>
+            <div class = "post-info profile-right">
+                <p class = "study-group">STUDY GROUP</p>
+                <h1>TITLE</h1>
+                <p class = "brief-descript">Brief Description</p>
+                <p class = "date-room"><i class="far fa-calendar"></i> Date</p>
+                <p class = "post-time-address align-with-icon">time</p>
+                <p class = "add-to-calendar align-with-icon">Add to calendar</p>
+                <p><i class = "date-room material-icons">location_on</i>Room</p>
+                <p class = "post-time-address align-with-icon">Address</p>
+            </div>
+            <div class = "join-section profile-right">
+                <h2 class = "join-button">Join</h2>
+            </div>
 
+        </div>
     </div>
     );
 //}
