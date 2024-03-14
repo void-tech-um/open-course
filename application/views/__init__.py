94e1e18dd6f801bf3af1216fec44651ecce4b182
Jennifer Weir <jennweir@umich.edu>
2024-03-13 21:22:37 -0400

    cleaned out lofi pages and condensed navbar

--- application/views/__init__.py
+++ application/views/__init__.py
@@ -1,7 +1,6 @@
 """Views, one for each Insta485 page."""
 from application.views.index import show_index
 from application.views.courses import show_courses
-from application.views.create import show_create
 from application.views.join import show_join
 from application.views.profile import show_profile
 from application.views.viewUsers import show_view_users
