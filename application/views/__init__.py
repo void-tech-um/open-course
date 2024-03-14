8affa8039078e65d376c8575c04050124c531786
Jennifer Weir <jennweir@umich.edu>
2024-03-13 21:25:40 -0400

    refactored viewing a user profile

--- application/views/__init__.py
+++ application/views/__init__.py
@@ -1,7 +1,6 @@
 """Views, one for each Insta485 page."""
 from application.views.index import show_index
 from application.views.courses import show_courses
-from application.views.join import show_join
 from application.views.profile import show_profile
-from application.views.viewUsers import show_view_users
+from application.views.view_user_profile import show_view_user_profile
 from application.views.saved import show_saved
--- application/views/viewUsers.py
+++
@@ -1,7 +0,0 @@
-import flask, application
-from flask import render_template
-
-
-@application.app.route('/viewUsers/')
-def show_view_users():
-    return render_template('viewUsers.html')
