3fd32237b29b7c65ba6e6471770db92680c6a713
Jennifer Weir <jennweir@umich.edu>
2024-04-03 19:20:27 -0400

    added route to connect to privacy policy

--- application/views/__init__.py
+++ application/views/__init__.py
@@ -6,3 +6,4 @@ from application.views.about import show_about
 from application.views.view_user_profile import show_view_user_profile
 from application.views.saved import show_saved
 from application.views.login import show_login
+from application.views.login import show_privacy_policy
