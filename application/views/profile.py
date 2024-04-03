8cd949e1f83f9c1a397a511b39f51fae8a1135d3
Jennifer Weir <jennweir@umich.edu>
2024-04-03 19:56:03 -0400

    added editable profile information

--- application/views/profile.py
+++ application/views/profile.py
@@ -11,3 +11,25 @@ def show_profile(username):
     # context = {course : [{course_code : EECS 280, course_name : INtroductory to Data structures},{course_code: EECS 370, course_name: }],
     #           posts : [{post_id: 4, title: "hi"}]}
     return render_template('profile.html', **context, profile_data=profile_data)
+
+
+def edit_profile_bio():
+    """Make an edit to a user's profile bio."""
+    if 'username' not in flask.session:
+        flask.abort(403)
+    try:
+        bio = flask.request.form['bio']
+        phone_num = flask.request.form['phone-num']
+    except KeyError:
+        flask.abort(400)
+
+    conn = application.model.get_db()
+    # insert into the database with the updated bio info
+    cur = conn.cursor()
+    cur.execute(
+        "UPDATE users "
+        "SET bio = %s, phone_num = %s "
+        "WHERE username = %s",
+        (bio, phone_num)
+    )
+    conn.commit()
