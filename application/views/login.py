517fd4f6a6898660545b88810de5030d04ce48fa
Ting Lu <tinglu@umich.edu>
2024-06-21 12:38:58 -0400

    first push with oauth

--- application/views/login.py
+++ application/views/login.py
@@ -1,10 +1,35 @@
-import flask, application
-from flask import render_template
+from flask import Flask, render_template, url_for, redirect, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from .. import google, oauth
+import application
 
-@application.app.route('/login/')
+
+@application.app.route('/show-login/')
 def show_login():
     return render_template('login.html')
 
 @application.app.route('/privacy-policy/')
 def show_privacy_policy():
     return render_template('privacy-policy.html')
+
+@application.app.route('/login')
+def login():
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+@application.app.route('/login/authorized')
+def authorized():
+    response = google.authorized_response()
+    if response is None or response.get('access_token') is None:
+        return 'Access denied: reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (response['access_token'], '')
+    user_info = google.get('userinfo')
+    return redirect(url_for('show_index'))
+
+@application.app.route('/logout')
+def logout():
+    session.pop('google_token')
+    return redirect(url_for('show_index'))
+
--- application/views/profile.py
+++ application/views/profile.py
@@ -1,13 +1,17 @@
 import flask, application
 from flask import render_template
-
+from .. import google
 @application.app.route("/profile/<username>/")
 def show_profile(username):
     """Display /profile route."""
+    user_info = google.get('userinfo')
+    print(user_info.data)
     profile_data = application.model.get_user(username)
     context = application.model.get_courses_of_user(username)
     post = application.model.get_posts_created_by_user(username)
-    context["posts"] = post["posts"]
+    context["name"] = user_info.data["name"]
+    context["profile_pic"] = user_info.data["picture"]
+    print(context["name"])
     # context = {course : [{course_code : EECS 280, course_name : INtroductory to Data structures},{course_code: EECS 370, course_name: }],
     #           posts : [{post_id: 4, title: "hi"}]}
     return render_template('profile.html', **context, profile_data=profile_data)
