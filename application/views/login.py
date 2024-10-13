8c98744ac4b87dbcc6fdf8fa256eafd7cc96b02c
Ting Lu <tinglu30@gmail.com>
2024-10-13 09:48:07 -0400

    added deployment for google oauth

--- application/views/login.py
+++ application/views/login.py
@@ -3,6 +3,7 @@ from flask_oauthlib.client import OAuth
 from application.model import get_user, add_user
 from .. import google, oauth
 import application
+import os
 
 
 @application.app.route('/show-login/')
@@ -19,8 +20,12 @@ def show_privacy_policy():
 @application.app.route('/login')
 def login():
     print(url_for('authorized', _external=True))
-    #return google.authorize_redirect(url_for('authorized', _external=True, _scheme='https')) NEED TO RUN THIS IN DEPLOYMENT
-    return google.authorize_redirect(url_for('authorized', _external=True, _scheme='https'))
+    if os.getenv('FLASK_ENV') == 'production':
+        # Use HTTPS for production
+        return google.authorize_redirect(url_for('authorized', _external=True, _scheme='https'))
+    else:
+        # Use HTTP for local development
+        return google.authorize_redirect(url_for('authorized', _external=True))
 
 
 @application.app.route('/login/authorized')
